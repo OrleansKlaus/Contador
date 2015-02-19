8f71a251dffeb8de0ca2d9002161302e7906d53e
OrleansKlaus <orleansklaus@gmail.com>
2015-02-19 09:06:30 -0200

    Voltado o nome

--- ContaPessoas/SecondViewController.m
+++ ContaPessoas/SecondViewController.m
@@ -2,7 +2,7 @@
 //  SecondViewController.m
 //  ContaPessoas
 //
-//  Created by Orleans Klaus on 2/18/15.
+//  Created by Vinicius Viana on 2/18/15.
 //  Copyright (c) 2015 Vinicius Miana. All rights reserved.
 //
 
